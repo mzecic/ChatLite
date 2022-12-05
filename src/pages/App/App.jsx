86453d5592244f17cb92d91bb563b6be24b450b4
mzecic <matejzecic@gmail.com>
2022-12-05 16:35:37 -0500

    deploy

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
 import './App.css';
 import { getUser } from '../../utilities/users-service'
 import AuthPage from '../AuthPage/AuthPage'
 import NavBar from '../../components/NavBar/NavBar'
 import MobileNavBar from '../../components/MobileNavBar/MobileNavBar';
 import InboxPage from '../InboxPage/InboxPage'
-import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import * as userService from '../../utilities/users-service'
 
 export default function App() {
@@ -25,10 +23,6 @@ export default function App() {
           <NavBar handleLogOut={handleLogOut} user={user} setUser={setUser} />
           <MobileNavBar handleLogOut={handleLogOut} user={user} navBar={navBar} setNavBar={setNavBar} />
           <InboxPage user={user} navBar={navBar} setNavBar={setNavBar} />
-          <Routes>
-            {/* <Route path="/signup" element={<SignUpForm />} /> */}
-            {/* <Route path="/inbox" element={<InboxPage user={user}/>}/> */}
-          </Routes>
         </>
         :
         <AuthPage setUser={setUser}/>
